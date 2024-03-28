c6820ec699bdd4310b610c0ccdfc81f3557d6e8b
tomasesber <tomas.esber@duke.edu>
2024-03-28 14:09:28 -0400

    Create XCode Project

---
+++ mochilla-hackathon/mochilla-hackathon/mochilla_hackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  mochilla_hackathonApp.swift
+//  mochilla-hackathon
+//
+//  Created by Tomas Esber on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct mochilla_hackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
